22e56aa487f397494369e3092d9413683967f05b
Denis Silva <denis1206@hotmail.com>
2019-04-17 17:26:58 -0300

    Desafio básico do dia 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import SquareCollab from './components/SquareCollab/index';
+import BoardCollab from './components/BoardCollab/index'
 
-const App = () => <SquareCollab />
+const App = () => (
+    <>
+        <BoardCollab />
+        <SquareCollab />
+    </>
+)
 
 export default App;
