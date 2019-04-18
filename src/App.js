fca2a014d4ad9704713e539a2b0f1d9b86cd974f
Denis Silva <denis1206@hotmail.com>
2019-04-18 13:46:03 -0300

    desafio basico dia 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import BoardCollab from './components/BoardCollab/index'
+import NextPlayer from './components/NextPlayer/index'
 
-const App = () => <BoardCollab qtd="9" />
+const App = () => (
+    <>
+        <NextPlayer />
+        <BoardCollab qtd="9" />
+    </>
+)
 
 export default App;
---
+++ src/components/NextPlayer/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const NextPlayer = () => (<p>Próximo Jogador: X</p>)
+
+export default NextPlayer;
