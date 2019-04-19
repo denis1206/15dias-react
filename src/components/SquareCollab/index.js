1f6b7c0c72872731d7ca8c4587cb5a4cb6b5869f
Dênis Silva <denis1206@hotmail.com>
2019-04-19 19:20:56 -0300

    desafio avançado dia 3

--- src/components/SquareCollab/index.js
+++ src/components/SquareCollab/index.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './styles.css';
- 
-const handleClick = texto => () => console.log(texto)
 
-const SquareCollab = () => (
-    <button onClick={handleClick("coiso")} className="square-collab">X</button>
-);
+class SquareCollab extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            content: "X"
+        }
+    }
+
+    handleClick = () => {
+        const newState = {}
+        this.state.content === "X" ? newState.content = "0" : newState.content = "X"
+        this.setState(newState)
+    }
+
+    render(){
+        return (<button onClick={this.handleClick} className="square-collab">{this.state.content}</button>)
+    }
+}
 
 export default SquareCollab;
